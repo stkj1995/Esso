960fb3589681aa77395308b650795c4e3e550cce
Ladan <lada0080@stud.kea.dk>
2025-05-13 13:10:01 +0200

    nyt designsystem uden fonte

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,58 +5,61 @@ export default {
   /* Vil du overskrive eller extende tailwinds indstillinger?  */
 
   /* Primitives - Mode 1 */
-  module,
-  exports: {
-    theme: {
-      extend: {
-        colors: {
-          "color-hvid": "#FFFFFFFF",
-          "color-creme": "#F7F0E6FF",
-          "color-lysebrun": "#703F1DFF",
-          "color-mørkebrun": "#4F2C13FF",
-        },
+  theme: {
+    fontFamily: {
+      //     sans: ['Prompt', 'sans-serif'],
+      //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
+      //
+    },
 
-        border: {
-          "border-1": "1px",
-          "border-2": "2px",
-        },
+    extend: {
+      colors: {
+        "color-hvid": "#FFFFFFFF",
+        "color-creme": "#F7F0E6FF",
+        "color-lysebrun": "#703F1DFF",
+        "color-mørkebrun": "#4F2C13FF",
+      },
 
-        border_radius: {
-          "border-radius-0": "0px",
-          "border-radius-25": "25px",
-          "border-radius-30": "30px",
-          "border-radius-40": "40px",
-          "border-radius-50": "50px",
-        },
+      border: {
+        "border-1": "1px",
+        "border-2": "2px",
+      },
 
-        spacing: {
-          "spacing-xs-(mobil)": "10px",
-          "spacing-s-(mobil)": "15px",
-          "spacing-sm-(mobil)": "18px",
-          "spacing-m-(mobil)": "20px",
-          "spacing-ml-(mobil)": "30px",
-          "spacing-lg-(mobil)": "35px",
-          "spacing-xl-(mobil)": "40px",
-          "spacing-2xl-(mobil)": "60px",
-          "spacing-3xl-(mobil)": "70px",
-          "spacing-4xl-(mobil)": "100px",
-          "spacing-5xl-(mobil)": "120px",
-          "spacing-6xl-(mobil)": "150px",
-          "spacing-xs": "10px",
-          "spacing-s": "20px",
-          "spacing-sm": "30px",
-          "spacing-m": "40px",
-          "spacing-ml": "48px",
-          "spacing-lg": "65px",
-          "spacing-xl": "80px",
-          "spacing-2xl": "90px",
-          "spacing-3xl": "120px",
-          "spacing-4xl": "130px",
-          "spacing-5xl": "142px",
-          "spacing-6xl": "200px",
-          "spacing-7xl": "240px",
-          "spacing-8xl": "280px",
-        },
+      borderadius: {
+        "border-radius-0": "0px",
+        "border-radius-25": "25px",
+        "border-radius-30": "30px",
+        "border-radius-40": "40px",
+        "border-radius-50": "50px",
+      },
+
+      spacing: {
+        "spacing-xs-(mobil)": "10px",
+        "spacing-s-(mobil)": "15px",
+        "spacing-sm-(mobil)": "18px",
+        "spacing-m-(mobil)": "20px",
+        "spacing-ml-(mobil)": "30px",
+        "spacing-lg-(mobil)": "35px",
+        "spacing-xl-(mobil)": "40px",
+        "spacing-2xl-(mobil)": "60px",
+        "spacing-3xl-(mobil)": "70px",
+        "spacing-4xl-(mobil)": "100px",
+        "spacing-5xl-(mobil)": "120px",
+        "spacing-6xl-(mobil)": "150px",
+        "spacing-xs": "10px",
+        "spacing-s": "20px",
+        "spacing-sm": "30px",
+        "spacing-m": "40px",
+        "spacing-ml": "48px",
+        "spacing-lg": "65px",
+        "spacing-xl": "80px",
+        "spacing-2xl": "90px",
+        "spacing-3xl": "120px",
+        "spacing-4xl": "130px",
+        "spacing-5xl": "142px",
+        "spacing-6xl": "200px",
+        "spacing-7xl": "240px",
+        "spacing-8xl": "280px",
       },
     },
   },
